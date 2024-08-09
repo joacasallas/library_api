74cb2358be73af4d7118375d31daccc7f094a8f8
joacasallas2 <proyecta.ds@gmail.com>
2024-08-08 22:43:46 -0500

    Create class Book

--- models.py
+++ models.py
@@ -19,3 +19,12 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.username}>'
+
+class Book(db.Model):
+    """This class rerpesent a book instance"""
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
